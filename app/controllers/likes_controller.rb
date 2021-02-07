0ce7d276f4309934f6b46c90e7177dc59e6bf130
nyancho-inm <75053805+nyancho-inm@users.noreply.github.com>
2021-02-07 09:22:17 +0900

    非同期いいね実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,16 @@
 class LikesController < ApplicationController
+  before_action :post_params
 
   def create
     Like.create(user_id: current_user.id, post_id: params[:id])
-    redirect_to posts_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, post_id: params[:id]).destroy
-    redirect_to posts_path
   end
-  
+
+  def post_params
+    @post = Post.find(params[:id])
+  end
+
 end
